aa58769a70f177b2c11cd5a6c101110d114b0735
Thinline20 <thinline20@gmail.com>
2022-03-31 20:59:47 +0900

    implement signup page

--- js/signup.js
+++ js/signup.js
@@ -15,6 +15,26 @@ const isUserAuthorized = async (loginInfo) => {
   }
 };
 
+let studentDateData = document.querySelectorAll(".student-form-data");
+let signupRole = document.getElementById("signup-role");
+signupRole.selectedIndex = -1;
+
+signupRole.addEventListener("change", (e) => {
+  if (e.target.value === "student") {
+    for (let node of studentDateData) {
+      if (node.classList.contains("hidden")) {
+        node.classList.remove("hidden");
+      }
+    }
+  } else {
+    for (let node of studentDateData) {
+      if (!node.classList.contains("hidden")) {
+        node.classList.add("hidden");
+      }
+    }
+  }
+});
+
 document.getElementById("signup-enter-date").addEventListener("change", (e) => {
   let gradDate = new Date(e.target.value);
   gradDate.setFullYear(gradDate.getFullYear() + 4);
