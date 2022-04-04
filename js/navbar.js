04a304237b3eeefb4cfc29df12fd41288eff32c9
Thinline20 <thinline20@gmail.com>
2022-04-04 11:25:52 +0900

    Fix dropdown item not visible when hover navbar item :

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,19 @@
 /**
  * @prettier
  */
+
+document.querySelectorAll(".top-navbar-main-item").forEach((element) => {
+  let dropdownItem = element.children[1];
+
+  element.addEventListener("mouseover", (_) => {
+    if (dropdownItem.classList.contains("hidden")) {
+      dropdownItem.classList.remove("hidden");
+    }
+  });
+
+  element.addEventListener("mouseout", (_) => {
+    if (!dropdownItem.classList.contains("hidden")) {
+      dropdownItem.classList.add("hidden");
+    }
+  });
+});
