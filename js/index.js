418ca162b04e51ee649dea05e268ab06b36115fb
Thinline20 <thinline20@gmail.com>
2022-04-04 22:39:49 +0900

    Set value for margin-top of searchbox

--- js/index.js
+++ js/index.js
@@ -7,10 +7,11 @@ let searchBox = document.querySelector(".search-form-wrapper");
 
 window.addEventListener("scroll", () => {
   let currentScrollTop = document.documentElement.scrollTop;
+  console.log(currentScrollTop)
 
-  if (currentScrollTop > 90) {
-    searchBox.style.marginTop = `max(calc(50vh - 15rem), calc(50vh - 5rem - ${currentScrollTop}px + 90px))`;
-  } else {
-    searchBox.style.marginTop = "calc(50vh - 5rem)";
+  if (currentScrollTop > 100) {
+    searchBox.style.marginTop = `max(calc(45vh - 15rem), calc(50vh - 5rem - ${currentScrollTop}px + 90px))`;
   }
+
+  lastScrollTop = currentScrollTop;
 });
