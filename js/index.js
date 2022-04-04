0ced93f840ba80651b56101ee643b1ffa67a39b2
Thinline20 <thinline20@gmail.com>
2022-04-04 13:24:57 +0900

    Make searchBox responsive to scroll

--- js/index.js
+++ js/index.js
@@ -8,6 +8,9 @@ let searchBox = document.querySelector(".search-form-wrapper");
 window.addEventListener("scroll", () => {
   let currentScrollTop = document.documentElement.scrollTop;
 
-  if (currentScrollTop > 80) {
+  if (currentScrollTop > 90) {
+    searchBox.style.marginTop = `max(calc(50vh - 15rem), calc(50vh - 5rem - ${currentScrollTop}px + 90px))`;
+  } else {
+    searchBox.style.marginTop = "calc(50vh - 5rem)";
   }
 });
