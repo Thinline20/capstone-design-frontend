cb7530c9451399d52b64e9c0aa51fb8f6657f258
Thinline20 <thinline20@gmail.com>
2022-04-04 11:44:48 +0900

    Implementing scroll stuff

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@
  * @prettier
  */
 
+let lastScrollTop = window.pageYOffset || document.body.scrollTop;
+let searchBox = document.querySelector(".search-form-wrapper");
+
+window.addEventListener("scroll", () => {
+  let currentScrollTop = document.documentElement.scrollTop;
+
+  if (currentScrollTop > 80) {
+  }
+});
