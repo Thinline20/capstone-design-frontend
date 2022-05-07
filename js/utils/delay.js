060e263569c8cae612036f20a2a469ecf60b36a0
Thinline20 <thinline20@gmail.com>
2022-05-08 00:41:44 +0900

    New delay function for setTimeout Chaining

---
+++ js/utils/delay.js
@@ -0,0 +1,15 @@
+/**
+ * @prettier
+ */
+
+export function delay(t, v) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve.bind(null, v), t);
+  });
+}
+
+Promise.prototype.delay = function (t) {
+  return this.then(function (v) {
+    return delay(t, v);
+  });
+};
